f80835900ad25da61c113156f9d93d5a0ae5c49a
ikrotzky <ikr@croworc.com>
2020-09-16 11:11:54 +0200

    [master] docs: Fix comment in VideosRepository

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -40,8 +40,8 @@ class VideosRepository(private val database: VideosDatabase) {
     /**
      * Refresh the videos stored in the offline cache.
      *
-     * This function uses the IO dispatcher to ensure the database insert database operation
-     * happens on the IO dispatcher. By switching to the IO dispatcher using `withContext` this
+     * This function uses the IO dispatcher to ensure the database insert operation
+     * happens on the IO dispatcher. By switching to the IO dispatcher using `withContext`, this
      * function is now safe to call from any thread including the Main thread.
      *
      * To actually load the videos for use, observe [videos]
